02cc1881a3d833fe293aaa9f67fd8a14f63ac7c3
Nissan <nissy34@gmail.com>
2020-06-02 22:05:27 +0300

    Update user.py

--- DB/models/user.py
+++ DB/models/user.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=True)
+    password = db.Column(db.String(255), nullable=False)
 
     receivedMessages = db.relationship("MessageReceiver", back_populates="receiver", cascade="all")
     sentMessagas = db.relationship('Message', back_populates="sender", cascade="all")
