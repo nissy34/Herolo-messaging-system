8c829621a21a493daa610bdc0344e648dc4af6b1
Nissan <nissy34@gmail.com>
2020-06-02 21:45:07 +0300

    Fix dir logs

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.7.3
 
 WORKDIR /usr/src/app
 
-RUN pip install gunicorn
+RUN mkdir logs
 
 COPY requirements.txt .
 
--- logger/logger.py
+++ logger/logger.py
@@ -5,5 +5,3 @@ import yaml
 with open('logger/config.yaml', 'r') as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
-
-
